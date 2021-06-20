23ff5f2b3a88a7a95d3e0a6fde96a0c81912663a
anton7r <anton.rosnell@elisanet.fi>
2021-06-20 18:02:37 +0300

    added a way to prepare queries

---
+++ internal/lexer/prepare.go
@@ -0,0 +1,87 @@
+package lexer
+
+import (
+	"unicode/utf8"
+)
+
+type Part struct {
+	Str     string
+	OnParam bool
+}
+
+type PartNamed struct {
+	Str     string
+	OnParam bool
+}
+
+//PrepareNamed is used for parsing and preparing sql queries
+func PrepareNamed(sql string) (*[]PartNamed, error) {
+
+	sl := []PartNamed{} //slice
+
+	start := 0
+	pos := 0
+	onParameter := false
+
+	for {
+		r, width := utf8.DecodeRuneInString(sql[pos:])
+
+		if r == utf8.RuneError {
+			sl = append(sl, PartNamed{sql[start:pos], onParameter})
+
+			break
+		} else if r == delim {
+			sl = append(sl, PartNamed{sql[start:pos], onParameter})
+
+			onParameter = true
+			start = pos + width
+		} else if r >= 'z' || r <= '1' {
+			if onParameter {
+				sl = append(sl, PartNamed{sql[start:pos], onParameter})
+
+				onParameter = false
+				start = pos
+			}
+		}
+
+		pos += width
+	}
+
+	return &sl, nil
+}
+
+//Prepare is used for parsing and preparing sql queries
+func Prepare(sql string) (*[]Part, error) {
+
+	sl := []Part{} // slice
+
+	start := 0
+	pos := 0
+	onParameter := false
+
+	for {
+		r, width := utf8.DecodeRuneInString(sql[pos:])
+
+		if r == utf8.RuneError {
+			sl = append(sl, Part{sql[start:pos], onParameter})
+
+			break
+		} else if r == '$' {
+			sl = append(sl, Part{sql[start:pos], onParameter})
+
+			onParameter = true
+			start = pos + width
+		} else if r >= 'z' || r <= '1' {
+			if onParameter {
+				sl = append(sl, Part{sql[start:pos], onParameter})
+
+				onParameter = false
+				start = pos
+			}
+		}
+
+		pos += width
+	}
+
+	return &sl, nil
+}
--- prepare.go
+++ prepare.go
@@ -1,8 +1,93 @@
 package pgxe
 
+import (
+	"github.com/anton7r/pgxe/internal/lexer"
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+)
+
 type PreparedQuery struct {
+	parts *[]lexer.Part
 }
 
-func Prepare(sql string) *PreparedQuery {
-	return &PreparedQuery{}
+//Prepares a traditional SQL-query with $1, $2, $3 placeholders
+//NOTE: This does not use the pgx way to prepare queries under the hood
+func Prepare(sql string) (*PreparedQuery, error) {
+	parts, err := lexer.Prepare(sql)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &PreparedQuery{
+		parts,
+	}, nil
+}
+
+func (p *PreparedQuery) toStr(args ...interface{}) string {
+	//TODO:
+	return ""
+}
+
+func (p *PreparedQuery) Select(db DB, target *interface{}, args ...interface{}) error {
+	return db.Select(target, p.toStr(args))
+}
+
+func (p *PreparedQuery) Get(db DB, target *interface{}, args ...interface{}) error {
+	return db.Get(target, p.toStr(args))
+}
+
+func (p *PreparedQuery) Query(db DB, args ...interface{}) (pgx.Rows, error) {
+	return db.Query(p.toStr(args))
+}
+
+func (p *PreparedQuery) QueryRow(db DB, args ...interface{}) pgx.Row {
+	return db.QueryRow(p.toStr(args))
+}
+
+func (p *PreparedQuery) Exec(db DB, args ...interface{}) (pgconn.CommandTag, error) {
+	return db.Exec(p.toStr(args))
+}
+
+type PreparedNamedQuery struct {
+	parts []lexer.PartNamed
+}
+
+//Prepares a named SQL-query with :FieldName placeholders
+//NOTE: This does not use the pgx way to prepare queries under the hood
+func PrepareNamed(sql string) (*PreparedNamedQuery, error) {
+	parts, err := lexer.PrepareNamed(sql)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &PreparedNamedQuery{
+		*parts,
+	}, nil
+}
+
+func (p *PreparedNamedQuery) toStr(arg *interface{}) string {
+	//TODO:
+	return ""
+}
+
+func (p *PreparedNamedQuery) Select(db DB, target *interface{}, arg *interface{}) error {
+	return db.Select(target, p.toStr(arg))
+}
+
+func (p *PreparedNamedQuery) Get(db DB, target *interface{}, arg *interface{}) error {
+	return db.Get(target, p.toStr(arg))
+}
+
+func (p *PreparedNamedQuery) Exec(db DB, arg *interface{}) (pgconn.CommandTag, error) {
+	return db.Exec(p.toStr(arg))
+}
+
+func (p *PreparedNamedQuery) Query(db DB, arg *interface{}) (pgx.Rows, error) {
+	return db.Query(p.toStr(arg))
+}
+
+func (p *PreparedNamedQuery) QueryRow(db DB, arg *interface{}) pgx.Row {
+	return db.QueryRow(p.toStr(arg))
 }
