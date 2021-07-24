422322b728a77af5f30be5eee4da846c3eb36c09
anton7r <anton.rosnell@elisanet.fi>
2021-07-24 22:36:45 +0300

    more tests

--- pgxe_test.go
+++ pgxe_test.go
@@ -103,16 +103,45 @@ func TestGet(t *testing.T) {
 	}
 }
 
-//TODO
 func TestQuery(t *testing.T) {
+	rows, err := db.Query("SELECT * FROM employees")
 
+	if err != nil {
+		t.Error("Could't retrieve rows from db: " + err.Error())
+	}
+
+	for rows.Next() {
+		emp := Employee{}
+		rows.Scan(&emp.Name, &emp.Surname, &emp.PaymentAddress, &emp.ID)
+
+		if err != nil {
+			t.Error("Test errored:" + err.Error())
+			t.FailNow()
+		}
+
+		if emp.Name == "" {
+			t.Error("Name is empty")
+		}
+
+		if emp.Surname == "" {
+			t.Error("Surname is empty")
+		}
+
+		if emp.PaymentAddress == "" {
+			t.Error("PaymentAddress is empty")
+		}
+
+		if emp.ID == 0 {
+			t.Errorf("ID was 0")
+		}
+	}
 }
 
 func TestQueryRow(t *testing.T) {
-	emp := &Employee{}
+	emp := Employee{}
 
-	err := db.QueryRow("SELECT * FROM employees LIMIT 1").
-		Scan(emp)
+	obj := db.QueryRow("SELECT * FROM employees LIMIT 1")
+	err := obj.Scan(&emp.Name, &emp.Surname, &emp.PaymentAddress, &emp.ID)
 
 	if err != nil {
 		t.Error("Test errored:" + err.Error())
@@ -147,29 +176,130 @@ func TestExec(t *testing.T) {
 	t.Log("got command tag: " + c.String())
 }
 
-//TODO
+type tNamed struct {
+	Id int
+}
+
 func TestNamedSelect(t *testing.T) {
+	emps := []Employee{}
 
+	err := db.NamedSelect(&emps, "SELECT * FROM employees WHERE id <= :Id", &tNamed{Id: 3})
+	if err != nil {
+		t.Error("Test errored: " + err.Error())
+		t.FailNow()
+	}
+
+	if len(emps) != 3 {
+		t.Error("The length of the slice does not match expectations.")
+	}
 }
 
-//TODO
 func TestNamedGet(t *testing.T) {
+	emp := Employee{}
 
+	err := db.NamedGet(&emp, "SELECT * FROM employees WHERE id = :Id LIMIT 1", &tNamed{Id: 1})
+
+	if err != nil {
+		t.Error("Test errored:" + err.Error())
+		t.FailNow()
+	}
+
+	if emp.Name != "ABC" {
+		t.Error("Name wasnt as expected and got: " + emp.Name)
+	}
+
+	if emp.Surname != "123" {
+		t.Error("Surname wasnt as expected and got: " + emp.Surname)
+	}
+
+	if emp.PaymentAddress != "FooBar" {
+		t.Error("PaymentAddress wasnt as expected and got: " + emp.PaymentAddress)
+	}
+
+	if emp.ID != 1 {
+		t.Errorf("ID wasnt as expected and got: %o", emp.ID)
+	}
 }
 
-//TODO
 func TestNamedQuery(t *testing.T) {
+	rows, err := db.NamedQuery("SELECT * FROM employees WHERE ID <= :Id", &tNamed{Id: 3})
 
+	if err != nil {
+		t.Error("Could't retrieve rows from db: " + err.Error())
+	}
+
+	for rows.Next() {
+		emp := Employee{}
+		rows.Scan(&emp.Name, &emp.Surname, &emp.PaymentAddress, &emp.ID)
+
+		if err != nil {
+			t.Error("Test errored:" + err.Error())
+			t.FailNow()
+		}
+
+		if emp.Name == "" {
+			t.Error("Name is empty")
+		}
+
+		if emp.Surname == "" {
+			t.Error("Surname is empty")
+		}
+
+		if emp.PaymentAddress == "" {
+			t.Error("PaymentAddress is empty")
+		}
+
+		if emp.ID == 0 {
+			t.Errorf("ID was 0")
+		}
+	}
 }
 
-//TODO
 func TestNamedQueryRow(t *testing.T) {
+	emp := Employee{}
 
+	row, err := db.NamedQueryRow("SELECT * FROM employees WHERE id = :Id LIMIT 1", &tNamed{Id: 1})
+	if err != nil {
+		t.Error("Test errored:" + err.Error())
+		t.FailNow()
+	}
+
+	err = row.Scan(&emp.Name, &emp.Surname, &emp.PaymentAddress, &emp.ID)
+	if err != nil {
+		t.Error("Test errored 2:" + err.Error())
+		t.FailNow()
+	}
+
+	if emp.Name != "ABC" {
+		t.Error("Name wasnt as expected and got: " + emp.Name)
+	}
+
+	if emp.Surname != "123" {
+		t.Error("Surname wasnt as expected and got: " + emp.Surname)
+	}
+
+	if emp.PaymentAddress != "FooBar" {
+		t.Error("PaymentAddress wasnt as expected and got: " + emp.PaymentAddress)
+	}
+
+	if emp.ID != 1 {
+		t.Errorf("ID wasnt as expected and got: %o", emp.ID)
+	}
 }
 
-//TODO
-func TestNamedExec(t *testing.T) {
+type tNamedE struct {
+	Value string
+}
 
+func TestNamedExec(t *testing.T) {
+	c, err := db.NamedExec("INSERT INTO employees (name, surname, \"paymentAddress\") VALUES (:Value, :Value, :Value)", &tNamedE{Value: "Hi!"})
+
+	if err != nil {
+		t.Error("Insertion to the database failed: " + err.Error())
+		t.FailNow()
+	}
+
+	t.Log("got command tag: " + c.String())
 }
 
 func TestClose(t *testing.T) {
