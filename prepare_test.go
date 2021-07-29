8d22b6da189abe88a90042c9c7a30db65a9e894e
anton7r <anton.rosnell@elisanet.fi>
2021-07-30 00:18:45 +0300

    better coverage

--- prepare_test.go
+++ prepare_test.go
@@ -49,6 +49,39 @@ func TestPrepareGet(t *testing.T) {
 
 }
 
+func TestPrepareQuery(t *testing.T) {
+	prep := pgxe.Prepare("SELECT * FROM employees WHERE ID <= $1")
+
+	rows, err := prep.Query(db, 3)
+	
+	for rows.Next() {
+		emp := Employee{}
+		rows.Scan(&emp.Name, &emp.Surname, &emp.PaymentAddress, &emp.ID)
+
+		if err != nil {
+			t.Error("Test errored:" + err.Error())
+			t.FailNow()
+		}
+
+		if emp.Name == "" {
+			t.Error("Name is empty")
+		}
+
+		if emp.Surname == "" {
+			t.Error("Surname is empty")
+		}
+
+		if emp.PaymentAddress == "" {
+			t.Error("PaymentAddress is empty")
+		}
+
+		if emp.ID == 0 {
+			t.Errorf("ID was 0")
+		}
+	}
+}
+
+
 func TestPrepareQueryRow(t *testing.T) {
 	prep := pgxe.Prepare("SELECT * FROM employees WHERE ID = $1 LIMIT 1")
 
@@ -79,7 +112,37 @@ func TestPrepareQueryRow(t *testing.T) {
 	if emp.ID != 1 {
 		t.Errorf("ID wasnt as expected and got: %o", emp.ID)
 	}
+}
 
+func TestPrepareNamedGet(t *testing.T) {
+	prep, err := pgxe.PrepareNamed("SELECT * FROM employees WHERE ID = :Id LIMIT 1")
+
+	if err != nil {
+		t.Fatal("Errored in preparation: " + err.Error())
+	}
+
+	emp := Employee{}
+	
+	er2 := prep.Get(db, &emp, &tNamed{Id: 1})
+	if er2 != nil {
+		t.Error("Test errored: " + er2.Error())
+	}
+
+	if emp.Name != "ABC" {
+		t.Error("Name wasnt as expected and got: " + emp.Name)
+	}
+
+	if emp.Surname != "123" {
+		t.Error("Surname wasnt as expected and got: " + emp.Surname)
+	}
+
+	if emp.PaymentAddress != "FooBar" {
+		t.Error("PaymentAddress wasnt as expected and got: " + emp.PaymentAddress)
+	}
+
+	if emp.ID != 1 {
+		t.Errorf("ID wasnt as expected and got: %o", emp.ID)
+	}
 }
 
 func TestPrepareNamedQuery(t *testing.T) {
@@ -118,5 +181,41 @@ func TestPrepareNamedQuery(t *testing.T) {
 		if emp.ID == 0 {
 			t.Errorf("ID was 0")
 		}
+	}
+}
+
+func TestPrepareNamedQueryRow(t *testing.T) {
+	prep, err := pgxe.PrepareNamed("SELECT * FROM employees WHERE ID = :Id LIMIT 1")
+
+	if err != nil {
+		t.Fatal("Errored in preparation: " + err.Error())
+	}
+
+	row, er2 := prep.QueryRow(db, &tNamed{Id: 1})
+	if er2 != nil {
+		t.Error("Test errored: " + er2.Error())
+	}
+
+	emp := Employee{}
+	er3 := row.Scan(&emp.Name, &emp.Surname, &emp.PaymentAddress, &emp.ID)
+	if er3 != nil {
+		t.Error("Test errored:" + err.Error())
+		t.FailNow()
+	}
+
+	if emp.Name != "ABC" {
+		t.Error("Name wasnt as expected and got: " + emp.Name)
+	}
+
+	if emp.Surname != "123" {
+		t.Error("Surname wasnt as expected and got: " + emp.Surname)
+	}
+
+	if emp.PaymentAddress != "FooBar" {
+		t.Error("PaymentAddress wasnt as expected and got: " + emp.PaymentAddress)
+	}
+
+	if emp.ID != 1 {
+		t.Errorf("ID wasnt as expected and got: %o", emp.ID)
 	}
 }
